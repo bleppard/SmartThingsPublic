58299fa125e757122a915a3eec64ac7cb582103b
Benjamin Leppard <GitHub@public.leppard.com>
2019-02-03 20:12:30 -0800

    Change Name

--- devicetypes/smartthings/zigbee-rgbw-bulb.src/zigbee-rgbw-bulb.groovy
+++ devicetypes/smartthings/zigbee-rgbw-bulb.src/zigbee-rgbw-bulb.groovy
@@ -18,7 +18,7 @@
 import physicalgraph.zigbee.zcl.DataType
 
 metadata {
-	definition (name: "ZigBee RGBW Bulb", namespace: "smartthings", author: "SmartThings", runLocally: true, minHubCoreVersion: '000.019.00012', executeCommandsLocally: true) {
+	definition (name: "ZigBee RGBW Bulb - Enhanced", namespace: "smartthings", author: "Leppard", runLocally: true, minHubCoreVersion: '000.019.00012', executeCommandsLocally: true) {
 
 		capability "Actuator"
 		capability "Color Control"
